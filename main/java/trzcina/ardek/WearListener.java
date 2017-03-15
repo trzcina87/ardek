6e78197fe7887c72cda3da0b437f56bc2498795d
Piotr <piotr>
2017-03-15 18:01:16 +0100

    WEAR

--- main/java/trzcina/ardek/WearListener.java
+++ main/java/trzcina/ardek/WearListener.java
@@ -1,7 +1,5 @@
 package trzcina.ardek;
 
-import android.widget.Toast;
-
 import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.MessageEvent;
 
@@ -15,11 +13,11 @@ public class WearListener implements MessageApi.MessageListener {
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
-        showToast(messageEvent.getPath());
-    }
-
-    private void showToast(String message) {
-        Toast.makeText(activity, message, Toast.LENGTH_SHORT).show();
+        if(activity.sprawdzWifi()) {
+            Komunikat komunikat = new Komunikat((String) messageEvent.getPath(), System.currentTimeMillis());
+            activity.watek.dodajDoListy(komunikat);
+            activity.watek.interrupt();
+        }
     }
 
 }
